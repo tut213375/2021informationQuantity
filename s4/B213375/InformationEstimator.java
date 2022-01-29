8200b4a98967a21859d91803d40a900bbc304c3e
tut213375 <95679715+tut213375@users.noreply.github.com>
2022-01-29 14:04:58 +0900

    Style reformat and minor changes to debug output
    Removed or added some whitespaces/linebreaks.
    Minor content changes like removing unnecessary (double)casts or variable renaming.
    Edited comments form and sometimes content; removed some comments that I didn't find useful.
    Modified the text output when in debugmode (but not the information itself) to be more readable to me.
    For the test cases in main(), also added the expected output value (to confirm whether real output is correct).

--- s4/B213375/InformationEstimator.java
+++ s4/B213375/InformationEstimator.java
@@ -1,118 +1,124 @@
-package s4.B213375; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID. 
+package s4.B213375;
 import java.lang.*;
 import s4.specification.*;
 
 /* What is imported from s4.specification
 package s4.specification;
-public interface InformationEstimatorInterface {
+public interface InformationEstimatorInterface{
     void setTarget(byte target[]);  // set the data for computing the information quantities
     void setSpace(byte space[]);  // set data for sample space to computer probability
     double estimation();  // It returns 0.0 when the target is not set or Target's length is zero;
-    // It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
-    // The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
-    // Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-    // Otherwise, estimation of information quantity,
+        // It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
+        // The behavior is undefined, if the true value is finite but larger than Double.MAX_VALUE.
+        // Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
+        // Otherwise, estimation of information quantity.
 }
 */
 
-
-public class InformationEstimator implements InformationEstimatorInterface {
+public class InformationEstimator implements InformationEstimatorInterface{
     static boolean debugMode = false;
     // Code to test, *warning: This code is slow, and it lacks the required test
     byte[] myTarget; // data to compute its information quantity
     byte[] mySpace;  // Sample space to compute the probability
     FrequencerInterface myFrequencer;  // Object for counting frequency
 
-    private void showVariables() {
-	for(int i=0; i< mySpace.length; i++) { System.out.write(mySpace[i]); }
-	System.out.write(' ');
-	for(int i=0; i< myTarget.length; i++) { System.out.write(myTarget[i]); }
-	System.out.write(' ');
+    private void showVariables(){
+        System.out.print("spc:");
+        for(int i=0; i<mySpace.length; i++) System.out.write(mySpace[i]);
+        System.out.write(' ');
+
+        System.out.print("tgt:");
+        for(int i=0; i<myTarget.length; i++) System.out.write(myTarget[i]);
+        System.out.write(' ');
     }
 
-    byte[] subBytes(byte[] x, int start, int end) {
-        // corresponding to substring of String for byte[],
-        // It is not implement in class library because internal structure of byte[] requires copy.
-        byte[] result = new byte[end - start];
-        for(int i = 0; i<end - start; i++) { result[i] = x[start + i]; };
+    byte[] subBytes(byte[] x, int start, int end){
+        // similar to String.substring(start,end)
+        byte[] result=new byte[end-start];
+        for(int i=0; i<end-start; i++) result[i]=x[start+i];
         return result;
     }
 
-    // IQ: information quantity for a count, -log2(count/sizeof(space))
-    double iq(int freq) {
-        return  - Math.log10((double) freq / (double) mySpace.length)/ Math.log10((double) 2.0);
-    }
+    // IQ: Information Quantity for a count := -log2(count/|space|)
+    double iq(int freq){return -Math.log10((double)freq/mySpace.length)/Math.log10(2.0);}
 
     @Override
-    public void setTarget(byte[] target) {
-        myTarget = target;
-    }
-
+    public void setTarget(byte[] target){myTarget=target;}
     @Override
-    public void setSpace(byte[] space) {
+    public void setSpace(byte[] space){
         myFrequencer = new Frequencer();
-        mySpace = space; myFrequencer.setSpace(space);
+        myFrequencer.setSpace((mySpace=space));
     }
 
     @Override
-    public double estimation(){
-        boolean [] partition = new boolean[myTarget.length+1];
-        int np = 1<<(myTarget.length-1);
-        double value = Double.MAX_VALUE; // value = mininimum of each "value1".
-	if(debugMode) { showVariables(); }
-        if(debugMode) { System.out.printf("np=%d length=%d ", np, +myTarget.length); }
+    public double estimation(){//estimation of IQ
+        double output = Double.MAX_VALUE; // init @ worst case, then reduce if possible
+        boolean[] partition = new boolean[myTarget.length+1];
+        int np = 1<<(myTarget.length-1); // number of ways to partition myTarget : 2^(n-1)
 
-        for(int p=0; p<np; p++) { // There are 2^(n-1) kinds of partitions.
-            // binary representation of p forms partition.
-            // for partition {"ab" "cde" "fg"}
-            // a b c d e f g   : myTarget
-            // T F T F F T F T : partition:
-            partition[0] = true; // I know that this is not needed, but..
-            for(int i=0; i<myTarget.length -1;i++) {
-                partition[i+1] = (0 !=((1<<i) & p));
-            }
+        if(debugMode)showVariables();
+        if(debugMode)System.out.printf("np=%d length=%d\n", np, +myTarget.length);
+
+        for(int p=0; p<np; p++){
+            // boolean array canonical form of the p-th partition permutation
+            /* e.g. partition permutation {"ab" "cde" "fg"}
+                a b c d e f g   : myTarget[]
+                T F T F F T F T : partition[] (TRUE iff mT[i] starts a substring)
+            */
+            partition[0] = true;
+            for(int i=0; i<myTarget.length-1; i++)
+                partition[i+1] = (0!=((1<<i)&p)); //use binary to easily map all permutations
             partition[myTarget.length] = true;
 
-            // Compute Information Quantity for the partition, in "value1"
-            // value1 = IQ(#"ab")+IQ(#"cde")+IQ(#"fg") for the above example
-            double value1 = (double) 0.0;
-            int end = 0;
-            int start = end;
-            while(start<myTarget.length) {
-                // System.out.write(myTarget[end]);
-                end++;;
-                while(partition[end] == false) {
-                    // System.out.write(myTarget[end]);
+            // compute Information Quantity for this partition candidate as "value1"
+            /* e.g. {"ab" "cde" "fg"} -> value1 := IQ("ab")+IQ("cde")+IQ("fg") */
+            double value1 = 0.0;
+            int end = 0, start = 0;
+            while(start<myTarget.length){
+                if(debugMode)System.out.write(myTarget[end]);
+                end++;
+                while(partition[end]==false){
+                    if(debugMode)System.out.write(myTarget[end]);
                     end++;
                 }
-                // System.out.print("("+start+","+end+")");
+                if(debugMode)System.out.print("["+start+"~"+end+") ");
                 myFrequencer.setTarget(subBytes(myTarget, start, end));
-                value1 = value1 + iq(myFrequencer.frequency());
-                start = end;
+                value1 += iq(myFrequencer.frequency());
+                start=end;
             }
-            // System.out.println(" "+ value1);
+            if(debugMode)System.out.println("\tval_"+p+" := "+ value1);
 
-            // Get the minimal value in "value"
-            if(value1 < value) value = value1;
+            // retain the minimal value
+            if(value1<output) output=value1;
         }
-	if(debugMode) { System.out.printf("%10.5f\n", value); }
-        return value;
+        if(debugMode)System.out.printf("min_p = %10.5f\n", output);
+        return output;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         InformationEstimator myObject;
-        double value;
-	debugMode = true;
-        myObject = new InformationEstimator();
+        InformationEstimator.debugMode = true;
+        myObject=new InformationEstimator();
         myObject.setSpace("3210321001230123".getBytes());
+
         myObject.setTarget("0".getBytes());
-        value = myObject.estimation();
+        if(!debugMode) myObject.showVariables();
+        System.out.printf("%10.5f", myObject.estimation());
+        System.out.printf(" (expected %10.5f)\n\n", 2.0);
+
         myObject.setTarget("01".getBytes());
-        value = myObject.estimation();
+        if(!debugMode) myObject.showVariables();
+        System.out.printf("%10.5f", myObject.estimation());
+        System.out.printf(" (expected %10.5f)\n\n", 3.0);
+
         myObject.setTarget("0123".getBytes());
-        value = myObject.estimation();
+        if(!debugMode) myObject.showVariables();
+        System.out.printf("%10.5f", myObject.estimation());
+        System.out.printf(" (expected %10.5f)\n\n", 3.0);
+
         myObject.setTarget("00".getBytes());
-        value = myObject.estimation();
+        if(!debugMode) myObject.showVariables();
+        System.out.printf("%10.5f", myObject.estimation());
+        System.out.printf(" (expected %10.5f)\n\n", 4.0);
     }
 }
-
