7e747aa207c6b6eba465428ddf4c557ce2163ed7
tut213375 <95679715+tut213375@users.noreply.github.com>
2022-01-30 12:31:08 +0900

    comments

--- s4/B213375/InformationEstimator.java
+++ s4/B213375/InformationEstimator.java
@@ -68,7 +68,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
                 }
             }, considering all the possible permutations to subdivide Space.*/
         double output = Double.MAX_VALUE; // init @ worst case, then reduce if possible
-        boolean[] partition = new boolean[myTarget.length+1]; // binary uniquely maps a unique p-th permutation
+        boolean[] partition = new boolean[myTarget.length+1]; // used to define a permutation
         int np = 1<<(myTarget.length-1); // number of ways to partition myTarget : 2^(n-1)
 
         if(debugMode)showVariables();
@@ -82,7 +82,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
             */
             partition[0] = true;
             for(int i=0; i<myTarget.length-1; i++)
-                partition[i+1] = (0!=((1<<i)&p));  // binary uniquely maps a unique p-th permutation
+                partition[i+1] = (0!=((1<<i)&p)); // binary uniquely maps a unique p-th permutation
             partition[myTarget.length] = true;
 
             // compute Information Quantity for this partition candidate as "value1"
